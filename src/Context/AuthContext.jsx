317acad994fe9bc74af0b2ef45915685af58046e
Ojasvi Sethi <ojasvisethi2407@gmail.com>
2023-08-02 07:32:45 +0530

    Update

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -43,7 +43,7 @@ export default function AuthProvider({ children }) {
   async function handleSubmit(email, password) {
     console.log(API);
     try {
-      const response = await fetch(`http://${API}:3000/auth/login`, {
+      const response = await fetch(`https://${API}:3000/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
